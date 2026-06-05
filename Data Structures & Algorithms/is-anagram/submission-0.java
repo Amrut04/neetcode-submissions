5accdc191c8dd7963f8ee5d9007ab1f3d329e0dd
Amrut04 <92541635+Amrut04@users.noreply.github.com>
2026-06-05 17:21:10 +0530

    Bulk sync: 11 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        String a;
+
+        for (int i = s.length() -1 ; i<0;i--){
+            a += s.charAt(i);
+        }
+
+        if(t.equals(s)){
+            return true;
+        }
+
+        return false;
+    }
+}
