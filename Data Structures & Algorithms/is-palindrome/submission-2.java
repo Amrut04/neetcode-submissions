7b68f47cfc75cfb2b43f45b3dd3ab43f3eedb200
Amrut04 <92541635+Amrut04@users.noreply.github.com>
2026-06-12 18:41:46 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String cleaned = s.toLowerCase().replaceAll("[^a-z0-9]","");
+        String reverse = new StringBuilder(cleaned).reverse().toString();
+
+        return cleaned.equals(reverse);
+    }
+}
