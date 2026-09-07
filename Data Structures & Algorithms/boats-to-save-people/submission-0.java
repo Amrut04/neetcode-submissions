20e2e80c56b140444153eabffcf741d19b2b1288
Amrut04 <92541635+Amrut04@users.noreply.github.com>
2026-09-07 21:12:42 +0530

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int j = people.length -1;
+        int i=0;
+        int ans =0;
+        while(i<=j){
+            ans ++;
+            if(people[i] + people[j] <= limit){
+                i++;
+            }
+            j--;
+        }
+        return ans;
+    }
+}
