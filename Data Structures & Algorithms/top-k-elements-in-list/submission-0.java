b77a670e914df29f46931e95c9e9be7814c1684a
Amrut04 <92541635+Amrut04@users.noreply.github.com>
2026-09-18 22:56:34 +0530

    Update: top-k-elements-in-list - submission-0

--- Data Structures & Algorithms/top-k-elements-in-list/submission-0.java
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.java
@@ -1,30 +1,17 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer,Integer> map = new HashMap<>();
-        List<Integer>[] bucket = new List[nums.length+1];
-        for(int n : nums){
-            map.put(n,map.getOrDefault(n,0)+1);
+        Map<Integer,Integer> maps = new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+            maps.put(nums[i],maps.getOrDefault(nums[i],0)+1);
         }
+        List<Integer> keys = new ArrayList(maps.keySet());
+        keys.sort((a,b) -> maps.get(b) - maps.get(a));
 
-        for(int key : map.keySet()){
-            int freq = map.get(key);
-
-            if(bucket[freq] == null){
-                bucket[freq] = new ArrayList<>();
-            }
-            bucket[freq].add(key);
+        int[] result = new int[k];
+        for(int i=0;i<k;i++){
+            result[i]= keys.get(i);
         }
-
-        int[] res = new int[k];
-        int count = 0;
-
-        for(int pos = bucket.length -1 ; pos >=0 && count < k ; pos--){
-            if(bucket[pos] != null){
-                for(Integer int1 : bucket[pos]){
-                    res[count++] = int1;
-                }
-            }
-        }  
-        return res;       
+        return result;
     }
 }
